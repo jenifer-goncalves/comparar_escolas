64acadf3c062d373225afd41eae37efdabf4e53f
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-03-13 14:49:17 -0300

    MODIFY: Tentativa de tudo, fiz o meu "máximo", preciso estudar mais

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -26,42 +26,81 @@ class Senai {
 
 
 /* ===== OBTER ELEMENTOS ===== */
-const inputCidadeA = document.querySelector('.unidade__input unidadeA__cidade');
-const inputCodA = document.querySelector('.unidade__input unidadeA__codigo');
-const inputAnoA = document.querySelector('.unidade__input unidadeA__ano');
-const inputQtdeA = document.querySelector('.unidade__input unidadeA__cursos');
+// Uma das únicas coisas que eu entendi (nem sei se tá certo)
+// Elementos A
+const inputCidadeA = document.querySelector('.unidadeA__cidade');
+const inputCodA = document.querySelector('.unidadeA__codigo');
+const inputAnoA = document.querySelector('.unidadeA__ano');
+const inputQtdeA = document.querySelector('.unidadeA__cursos');
 
-const inputCidadeB = document.querySelector('.unidade__input unidadeB__cidade');
-const inputCodB = document.querySelector('.unidade__input unidadeB__codigo');
-const inputAnoB = document.querySelector('.unidade__input unidadeB__ano');
-const inputQtdeB = document.querySelector('.unidade__input unidadeB__cursos');
+// Elementos B
+const inputCidadeB = document.querySelector('.unidadeB__cidade');
+const inputCodB = document.querySelector('.unidadeB__codigo');
+const inputAnoB = document.querySelector('.unidadeB__ano');
+const inputQtdeB = document.querySelector('.unidadeB__cursos');
+
+// Resultados (aba "Aguardando criação...")
+const resultadoA = document.querySelector('.unidadeA__mensagem');
+const resultadoB = document.querySelector('.unidadeB__mensagem');
+
+// Os botões
+const btnA = document.querySelector('.unidadeA__instanciar');
+const btnB = document.querySelector('.unidadeB__instanciar');
 
 
 
 /* ===== FUNÇÃO DE VALIDAÇÃO ===== */
+const cidade = inputCidadeA.value
+const codigo = inputCodA.value
+const ano = inputAnoA.value
+const qtde = inputQtdeA.value
 
-// Estudar isso no almoço urgente!
-// Lógica: Aqui eu quero verificar todos os inputs, independente se é A ou B, pq aqui eu não vou adicionar
-function obterInfos(){
-  const inputs = document.querySelectorAll('.unidade__input');
-
-  if (isNaN(cid) || isNaN(cod) || isNaN(anoCons) || isNaN(qtCursos)) {
-    mostrarResultado('Por favor, preencha os campos.');
-    return
+// Lógica: Aqui eu quero verificar todos os inputs, independente se é A ou B, pq aqui eu vou mostrar a msg
+function obterInfos(cidade, codigo, ano, qtde){
+    if (cidade === '' || codigo === '' || ano === '' || qtde === '') {
+    return false
   } else {
-    mostrarResultado(`Escola registrada. Status atual: FECHADA`)
+    return true
   }
-}
+};
 
 
 /* ===== INSTANCIAR ESCOLA A ===== */
-const escolaA = new Senai(codigo, cidade, anoConstrucao, qtdeCursos);
+const instanciarA = () => {
+  const inputCidadeA = document.querySelector('.unidadeA__cidade').value;
+  const inputCodA = document.querySelector('.unidadeA__codigo').value;
+  const inputAnoA = document.querySelector('.unidadeA__ano').value;
+  const inputQtdeA = document.querySelector('.unidadeA__cursos').value;
+  if (obterInfos) {
+    resultadoA.textContent = `Por favor, preencha os campos.`;
+  } else {
+    resultadoA.textContent = `Escola registrada. Status atual: FECHADA`
+  }
 
+  btnA.addEventListener('click', () => {
+    const escolaA = new Senai(codigo, cidade, anoConstrucao, qtdeCursos);
+  })
+};
 
 
 /* ===== INSTANCIAR ESCOLA B ===== */
-const escolaB = new Senai(codigo, cidade, anoConstrucao, qtdeCursos);
+const instanciarB = () => {
+  const inputCidadeB = document.querySelector('.unidadeB__cidade').value;
+  const inputCodB = document.querySelector('.unidadeB__codigo').value;
+  const inputAnoB = document.querySelector('.unidadeB__ano').value;
+  const inputQtdeB = document.querySelector('.unidadeB__cursos').value;
+  if (obterInfos) {
+    resultadoB.textContent = `Por favor, preencha os campos.`;
+  } else {
+    resultadoB.textContent = `Escola registrada. Status atual: FECHADA`
+  }
 
+
+
+  btnA.addEventListener('click', () => {
+    const escolaB = new Senai(codigo, cidade, anoConstrucao, qtdeCursos);
+  })
+};
 /* ===== ABRIR ESCOLA ===== */
 
 
